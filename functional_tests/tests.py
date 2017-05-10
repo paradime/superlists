5c7aa38f6b93e6b945fd46c71563b26a327e52e4
paradime <brw4824@rit.edu>
2017-05-09 23:17:43 -0400

    Add multi user to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,6 +32,9 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         import time
         time.sleep(1)
+
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -43,5 +46,22 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: ' + second_task)
 
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
-        self.fail('Finish the Test!')
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNoIn('Buy peacock feathers', page_text)
+        self.assertNoIn('make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNoEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNoIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
