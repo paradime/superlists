53ca76b196355d99929a8df8fda47fcd9c1f5e8c
paradime <brw4824@rit.edu>
2017-05-09 22:58:57 -0400

    make functiona tests an app, use liveServer

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(10)
 
     def tearDown(self):
         self.browser.quit()
@@ -16,7 +16,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_start_a_list(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -30,6 +30,8 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+        import time
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -37,12 +39,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(second_task)
         inputbox.send_keys(Keys.ENTER)
 
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: ' + second_task)
 
 
         self.fail('Finish the Test!')
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-
