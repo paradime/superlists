d864b8570bf77cdd2046ae28e69577c76171fb04
paradime <brw4824@rit.edu>
2017-05-09 21:53:31 -0400

    Display new to-do items

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,20 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+import re
 
 class HomePageTest(TestCase):
+
+    @staticmethod
+    def remove_csrf(html_code):
+        csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+        return re.sub(csrf_regex, '', html_code)
+
+    def assertEqualExceptCSRF(self, html_code1, html_code2):
+        return self.assertEqual(
+            self.remove_csrf(html_code1),
+            self.remove_csrf(html_code2)
+        )
 
     def test_root_url_resolves_home_page_view(self):
         found = resolve('/')
@@ -13,5 +25,20 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqualExceptCSRF(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+                'home.html',
+                {'new_item_text': 'A new list item'},
+                request=request,
+        )
+        self.assertEqualExceptCSRF(response.content.decode(), expected_html)
